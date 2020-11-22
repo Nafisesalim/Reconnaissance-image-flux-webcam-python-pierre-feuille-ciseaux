286a33b91dc172941f52b08ca888fb9419af84cd
Nafisesalim <36531885+Nafisesalim@users.noreply.github.com>
2020-11-22 15:42:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import cv2 as cv
-import PIL
 import numpy as np
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+
 
 
 
